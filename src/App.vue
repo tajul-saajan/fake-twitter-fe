3fd8bcc83ab5e1ab3a7028af4c8b1f6f9ae78f60
Tajul Islam <tajul.islam@sigmasolutions.com.bd>
2023-11-13 01:12:04 +0600

    initail commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+    <div>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="text-2xl font-bold">hi</div>
+  </main>
+</template>
